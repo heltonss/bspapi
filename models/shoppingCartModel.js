26c8f0c65e458b70c76d45c399e2bfe8b2062380
Heltonss <helton@heltonsouza.com.br>
2017-09-14 01:05:57 -0300

    finished

--- models/shoppingCartModel.js
+++ models/shoppingCartModel.js
@@ -4,48 +4,49 @@ function ShoppingCartModelDAO(model) {
     this.model = model;
 };
 
-ShoppingCartModelDAO.prototype.create = function(data, callback) {
+ShoppingCartModelDAO.prototype.create = function (data, callback) {
     var model = new this.model(data);
-    model.save(function(err, result) {
+    model.save(function (err, result) {
         callback(err, result)
     });
 
 }
 
-ShoppingCartModelDAO.prototype.find = function(query, callback) {
+ShoppingCartModelDAO.prototype.find = function (query, callback) {
     this.model.find(query).exec(callback);
 
 }
 
-ShoppingCartModelDAO.prototype.findOne = function(_id, callback) {
+ShoppingCartModelDAO.prototype.findOne = function (_id, callback) {
     var query = { _id: _id }
     this.model.findOne(query).exec(callback);
 
 }
 
-ShoppingCartModelDAO.prototype.update = function(_id, data, callback) {
+ShoppingCartModelDAO.prototype.update = function (_id, data, callback) {
     var query = { _id: _id }
     this.model.update(query, data).exec(
-        function(err, result) {
+        function (err, result) {
             callback(err, result)
         })
 
 }
 
-ShoppingCartModelDAO.prototype.remove = function(_id, callback) {
+ShoppingCartModelDAO.prototype.remove = function (_id, callback) {
     var query = { _id: _id }
     this.model.remove(query).exec(
-        function(err, result) {
+        function (err, result) {
             callback(err, result)
         });
 
 }
 
-module.exports = function(mongoose) {
+module.exports = function (mongoose) {
     var shoppingCart = mongoose.model('shoppingcarts', {
         name: String,
         description: String,
-        price: Number
+        price: Number,
+        photos: Object
     });
 
     return new ShoppingCartModelDAO(shoppingCart);
